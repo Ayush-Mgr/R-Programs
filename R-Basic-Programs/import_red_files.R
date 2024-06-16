e1f345c5c5fa5cd34fda617d165f0515643335e7
Ayush-Mgr <yush_mgr@gmail.com>
2024-06-16 22:33:47 +0530

    ok

--- R-Basic-Programs/import_red_files.R
+++ R-Basic-Programs/import_red_files.R
@@ -5,5 +5,8 @@ head(csv_Data)
 View(csv_Data)
 names(csv_Data)=c("Diamond carat","Damond Clarity","Diamond price")# change the data's column names
 head(csv_Data)
-table_data<-read.table(file"file.table") # ,sep=' ' if data is seperated by space
+#table_data<-read.table(file"file.table") # ,sep=' ' if data is seperated by space
 head(table_Data)
+write.csv(csv_Data, file = "DIAMOND_data.csv", row.names = FALSE)
+csv_updated<- read.csv(file = "Diamond_data.csv",header = TRUE)
+head(csv_updated)
