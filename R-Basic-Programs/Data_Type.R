edb3b285a5d18a5c5f9363e40a89e734b7991871
Ayush-Mgr <yush_mgr@gmail.com>
2024-06-16 20:29:55 +0530

    setup

---
+++ R-Basic-Programs/Data_Type.R
@@ -0,0 +1,16 @@
+#data types
+Numeric_Datatype = 5.1
+Numeric_Datatype2 = 5
+Int_Datatype=5L
+char_Datatype='A'
+string_Datatype="str"
+Logical_Datatype=TRUE
+Logical_Datatype2=FALSE
+#print datatype
+typeof(Numeric_Datatype)
+typeof(Numeric_Datatype2)
+typeof(Int_Datatype)
+typeof(char_Datatype)
+typeof(string_Datatype)
+typeof(Logical_Datatype)
+typeof(Logical_Datatype2)
